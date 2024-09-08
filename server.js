694fcf7e4d725355986962e7add8ef264a56d165
RAQEEB212 <saneelmemon45@gmail.com>
2024-09-09 02:24:47 +0500

    more then two get requests and nodemon added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello, Raqeeb! Welcome to Express.js!');
 });
 
+app.get('/api/course',(req,res) => {
+    res.send([1,2,3,4,5,6,7]);
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
